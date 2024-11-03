1e225debbe6af92c525f06eb2b28bc7e89fba7f6
Daniel Stoddart <112119554+danmsto@users.noreply.github.com>
2024-11-03 20:57:26 +0000

    Fixed top bar spacing (sort of) and replaced font with one with ligatures

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,37 +1,34 @@
 import content from "../content/content";
 
 function Heading() {
-  var b = document.querySelector("body");
-  var bs = getComputedStyle(b);
+    var b = document.querySelector("body");
+    var bs = getComputedStyle(b);
 
-  const fadeEffect = () => {
-    var i = 0;
-    const change = () => {
-      var doc = document.getElementById("consoleEffect");
-      var color = [
-        bs.getPropertyValue("--background"),
-        bs.getPropertyValue("--text"),
-      ];
-      doc.style.color = color[i];
-      i = (i + 1) % color.length;
+    const fadeEffect = () => {
+        var i = 0;
+        const change = () => {
+            var doc = document.getElementById("consoleEffect");
+            var color = [bs.getPropertyValue("--background"), bs.getPropertyValue("--text")];
+            doc.style.color = color[i];
+            i = (i + 1) % color.length;
+        };
+        setInterval(change, 750);
     };
-    setInterval(change, 750);
-  };
 
-  return (
-    <div>
-      {"danmsto> " + content.siteHeaders.uni}
-      <br />
-      {"danmsto> " + content.siteHeaders.rnd}
-      <br />
-      {"danmsto> " + content.siteHeaders.cod}
-      <br />
-      {"danmsto> " + content.siteHeaders.dev1}
-      <span id="consoleEffect" onLoad={fadeEffect()}>
-        _
-      </span>
-    </div>
-  );
+    return (
+        <div>
+            {"danmsto> " + content.siteHeaders.uni}
+            <br />
+            {/* {"danmsto> " + content.siteHeaders.rnd}
+      <br /> */}
+            {"danmsto> " + content.siteHeaders.cod}
+            {/* <br />
+      {"danmsto> " + content.siteHeaders.dev1} */}
+            <span id="consoleEffect" onLoad={fadeEffect()}>
+                _
+            </span>
+        </div>
+    );
 }
 
 export default Heading;
