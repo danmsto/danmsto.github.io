a23969f5a3637837114147d5b258037fa7f5d6bd
Dan Stoddart <112119554+danmsto@users.noreply.github.com>
2024-11-21 21:37:03 +0000

    Fixed blinking caret and changed to █ (#29)

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,23 +1,29 @@
-import { ReactEventHandler } from "react";
+import { useEffect } from "react";
 import content from "../content/content";
 
 function Heading() {
-    var b = document.querySelector("body");
-    if (b === null) return <></>;
-    var bs = getComputedStyle(b);
-    if (bs === null) return <></>;
+    useEffect(() => {
+        const b = document.querySelector("body");
+        if (!b) return;
 
-    const fadeEffect: ReactEventHandler<HTMLSpanElement> = (_: React.SyntheticEvent<HTMLSpanElement, Event>) => {
-        var i = 0;
-        const change = () => {
-            var doc = document.getElementById("consoleEffect");
-            if (doc === null) return <></>;
-            var color = [bs.getPropertyValue("--background"), bs.getPropertyValue("--text")];
-            doc.style.color = color[i];
-            i = (i + 1) % color.length;
+        const bs = getComputedStyle(b);
+
+        const fadeEffect = () => {
+            let i = 0;
+            const change = () => {
+                const doc = document.getElementById("consoleEffect");
+                if (!doc) return;
+
+                const color = [bs.getPropertyValue("--background"), bs.getPropertyValue("--text")];
+                doc.style.color = color[i];
+                i = (i + 1) % color.length;
+            };
+
+            setInterval(change, 750);
         };
-        setInterval(change, 750);
-    };
+
+        fadeEffect();
+    }, []);
 
     return (
         <div>
@@ -28,9 +34,7 @@ function Heading() {
             {"DS> " + content.siteHeaders.cod}
             <br />
             {"DS> " + content.siteHeaders.dev1}
-            <span id="consoleEffect" onLoad={fadeEffect}>
-                _
-            </span>
+            <span id="consoleEffect">█</span>
         </div>
     );
 }
