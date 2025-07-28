d670214a2749ae1d7bf37a94024d5ea917214adf
Daniel Stoddart <112119554+danmsto@users.noreply.github.com>
2025-07-28 22:03:06 +0100

    Tidied up the createAccordionUrl method

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
         b.style.setProperty("--text", "white");
         b.style.setProperty("--buttoninteract", "#202d36");
         b.style.setProperty("--badge-invert-value", "1");
-        b.style.setProperty("--accordion-button-after", createAccordionButton(false));
+        b.style.setProperty("--accordion-button-after", createAccordionUrl(false));
         break;
       case 2:
         b.style.setProperty("--background", "white");
@@ -33,7 +33,7 @@ function App() {
         b.style.setProperty("--text", "black");
         b.style.setProperty("--buttoninteract", "#dad2bc");
         b.style.setProperty("--badge-invert-value", "0");
-        b.style.setProperty("--accordion-button-after", createAccordionButton(true));
+        b.style.setProperty("--accordion-button-after", createAccordionUrl(true));
         break;
       case 3:
         b.style.setProperty("--background", "mediumseagreen");
@@ -42,7 +42,7 @@ function App() {
         b.style.setProperty("--text", "black");
         b.style.setProperty("--buttoninteract", "#9DC7CC");
         b.style.setProperty("--badge-invert-value", "0");
-        b.style.setProperty("--accordion-button-after", createAccordionButton(true));
+        b.style.setProperty("--accordion-button-after", createAccordionUrl(true));
         break;
     }
   }, [theme]);
@@ -133,8 +133,7 @@ function App() {
   );
 }
 
-function createAccordionButton(isDark: boolean): string {
-  // return `data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='${(isDark ? "black" : "white")}'%3e%3cpath fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/%3e%3c/svg%3e`
+function createAccordionUrl(isDark: boolean): string {
     return `url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='${isDark ? "black" : "white"}'%3e%3cpath fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/%3e%3c/svg%3e\")`;
 }
 
