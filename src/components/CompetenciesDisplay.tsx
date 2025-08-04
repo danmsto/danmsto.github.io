a748255b7046289164891b9f67dc4ba8f6db158f
Dan Stoddart <112119554+danmsto@users.noreply.github.com>
2025-08-05 00:09:42 +0100

    Update/2025.3 (#35)
    * Simplified CSS file
    
    * Remove vertical padding when on smaller display

--- src/components/CompetenciesDisplay.tsx
+++ src/components/CompetenciesDisplay.tsx
@@ -4,7 +4,7 @@ import competencies from "../content/competencies.ts";
 const CompetenciesDisplay = () => {
     return (
         <Card className="bodySection mt-2 mb-0">
-            <CardHeader className="blackText">Competencies</CardHeader>
+            <CardHeader>Competencies</CardHeader>
             <ListGroup className="bodySection mb-0 bodySection">
                 {competencies.map(c => (
                     <ListGroup.Item className="competenciesItem blackText">{c.icon.char} {c.label}</ListGroup.Item>
